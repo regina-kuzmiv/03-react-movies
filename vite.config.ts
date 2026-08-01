824d6b6f5ad20b0ec11447b00e881c4370910689
regina <reginakuzmiv310@gmail.com>
2026-08-01 12:50:34 +0200

    fix vercel base path

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,5 @@ import react, { reactCompilerPreset } from "@vitejs/plugin-react";
 import babel from "@rolldown/plugin-babel";
 
 export default defineConfig({
-  base: "/03-react-movies/",
   plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
 });
